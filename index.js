256a7daebf5249fba69486446b2b6f1c2e0f720a
Resla2 <116928691+Resla2@users.noreply.github.com>
2022-12-29 09:32:30 -0500

    Creating and Inserting DOM Nodes Lab
    Completed DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+element.textContent = "You've changed what's on the screen!";
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent  = 'Javascript is the champion!'
